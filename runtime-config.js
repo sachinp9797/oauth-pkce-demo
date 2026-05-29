98f838b0ceec86c6d902f6504984fed8e976ee92
OAuth Sample App <test@example.com>
2026-05-29 12:53:31 +0530

    Updates

---
+++ runtime-config.js
@@ -0,0 +1,7 @@
+window.__APP_CONFIG__ = {
+  "clientId": "UnaDX-N8PkqijiAFvI5b2sBhZa7Y0rb5OKPuUZFXwy4",
+  "authorizeUrl": "https://staging.xola.com/api/authorize",
+  "tokenUrl": "https://staging.xola.com/api/token",
+  "userInfoUrl": "https://staging.xola.com/api/users/me",
+  "redirectUri": "http://localhost:8081/callback"
+};
